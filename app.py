c2bbf798152f038a814384d42ad60e821e7ac347
jgoel <jay@hotswap.app>
2022-04-16 13:42:36 -0400

    update

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import sys
 
 print("starting...")
 print("here is an arg:", sys.argv[1])
-time.sleep(30)
+sleep_time = 15
+print("sleeping for", sleep_time)
+time.sleep(sleep_time)
 print("done!")
 
 
