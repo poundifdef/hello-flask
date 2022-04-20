ce34c21891808ff54500a531e70319a19dcb371c
Jay Goel <jay@hotswap.app>
2022-04-20 16:47:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import sys
 
 print("starting...")
-print("here is an arg:", sys.argv[1])
+print("here is an arg: Hello there, ", sys.argv[1])
 sleep_time = 15
 print("sleeping for", sleep_time)
 time.sleep(sleep_time)
